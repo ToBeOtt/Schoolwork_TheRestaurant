f0757aa4caeccba794c90ad37399e54890607a9e
SimonJsv <124794619+SimonJsv@users.noreply.github.com>
2023-11-23 11:45:33 +0100

    Interfaces
    Interfaces for OrderService/Repository

---
+++ TheRestaurant/TheRestaurant.Common.Infrastructure/Interfaces/IOrderService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheRestaurant.Domain.Entities.OrderEntities;
+
+namespace TheRestaurant.Common.Infrastructure.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<Order> CreateOrderAsync(Order order);
+        Task<Order> GetOrderByIdAsync(int orderId);
+        Task<List<Order>> GetAllOrdersAsync();
+        Task UpdateOrderAsync(Order order);
+        Task DeleteOrderAsync(int orderId);
+    }
+
+}
---
+++ TheRestaurant/TheRestaurant.Common.Infrastructure/Repositories/OrderRepository/IOrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheRestaurant.Domain.Entities.OrderEntities;
+
+namespace TheRestaurant.Common.Infrastructure.Repositories.OrderRepository
+{
+    public interface IOrderRepository
+    {
+        Task<Order> CreateAsync(Order order);
+        Task<Order> GetByIdAsync(int orderId);
+        Task<List<Order>> GetAllAsync();
+        Task UpdateAsync(Order order);
+        Task DeleteAsync(int orderId);
+    }
+}
