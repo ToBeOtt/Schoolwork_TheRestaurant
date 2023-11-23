9d6dabeef235b3b3b2d15d3a2b9cf4006ca31ae1
SimonJsv <124794619+SimonJsv@users.noreply.github.com>
2023-11-23 11:46:08 +0100

    Implementation
    Implemented the Interfaces

---
+++ TheRestaurant/TheRestaurant.Application/Services/OrderServices/OrderService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheRestaurant.Common.Infrastructure.Interfaces;
+using TheRestaurant.Common.Infrastructure.Repositories.OrderRepository;
+using TheRestaurant.Domain.Entities.OrderEntities;
+
+namespace TheRestaurant.Application.Services.OrderServices
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderService(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<Order> CreateOrderAsync(Order order)
+        {
+            // Implement the logic to create an order (e.g., validation)
+            return await _orderRepository.CreateAsync(order);
+        }
+
+        public async Task<Order> GetOrderByIdAsync(int orderId)
+        {
+            return await _orderRepository.GetByIdAsync(orderId);
+        }
+
+        public async Task<List<Order>> GetAllOrdersAsync()
+        {
+            return await _orderRepository.GetAllAsync();
+        }
+
+        public async Task UpdateOrderAsync(Order order)
+        {
+            // Implement the logic to update an order (e.g., validation)
+            await _orderRepository.UpdateAsync(order);
+        }
+
+        public async Task DeleteOrderAsync(int orderId)
+        {
+            // Implement the logic to delete an order (e.g., validation)
+            await _orderRepository.DeleteAsync(orderId);
+        }
+    }
+}
---
+++ TheRestaurant/TheRestaurant.Common.Infrastructure/Repositories/OrderRepository/OrderRepository.cs
@@ -0,0 +1,54 @@
+﻿using Common.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheRestaurant.Domain.Entities.OrderEntities;
+
+namespace TheRestaurant.Common.Infrastructure.Repositories.OrderRepository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly RestaurantDbContext _dbContext;
+
+        public OrderRepository(RestaurantDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Order> CreateAsync(Order order)
+        {
+            _dbContext.Orders.Add(order);
+            await _dbContext.SaveChangesAsync();
+            return order;
+        }
+
+        public async Task<Order> GetByIdAsync(int orderId)
+        {
+            return await _dbContext.Orders.FindAsync(orderId);
+        }
+
+        public async Task<List<Order>> GetAllAsync()
+        {
+            return await _dbContext.Orders.ToListAsync();
+        }
+
+        public async Task UpdateAsync(Order order)
+        {
+            _dbContext.Entry(order).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int orderId)
+        {
+            var order = await _dbContext.Orders.FindAsync(orderId);
+            if (order != null)
+            {
+                _dbContext.Orders.Remove(order);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
