810de03d2a61365ac5819a6e29ece35c1a8c1622
Sheldozer <125883340+Sheldozer@users.noreply.github.com>
2023-11-27 10:44:30 +0100

    Updated the MenuItem entity, changed the name of it & added properties

--- TheRestaurant/TheRestaurant.Domain/Entities/Menu/Item.cs
+++ TheRestaurant/TheRestaurant.Domain/Entities/Menu/Item.cs
@@ -1,14 +1,17 @@
 ﻿namespace TheRestaurant.Domain.Entities.Menu
 {
-    public class MenuItem
+    public class Item
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsFoodItem { get; set; }
         public string Description { get; set; }
         public byte[] MenuPhoto { get; set; }
+        public bool IsDeleted { get; set; }
         public ICollection<MenuItemCategory> MenuItemCategories { get; set; }
         public ICollection<MenuItemAllergy> MenuItemAllergies { get; set; }
-        public MenuItem()
+        public Item()
         {
             
         }
