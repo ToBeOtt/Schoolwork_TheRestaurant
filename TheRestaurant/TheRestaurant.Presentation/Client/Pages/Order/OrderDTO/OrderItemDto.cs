6a50c46ab85a1e8d235df375bf23ce2f706aa39d
SimonJsv <124794619+SimonJsv@users.noreply.github.com>
2023-11-28 15:36:06 +0100

    Made OrderItemDto for mocking purposes

---
+++ TheRestaurant/TheRestaurant.Presentation/Client/Pages/Order/OrderDTO/OrderItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TheRestaurant.Presentation.Client.Pages.Order.OrderDTO
+{
+    public class OrderItemDto
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+    }
+
+}
