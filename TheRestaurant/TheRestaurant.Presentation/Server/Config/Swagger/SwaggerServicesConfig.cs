6de34eba123ddb700e03a95789ba6d88c72dda4d
znooker <znooker_18@hotmail.com>
2023-11-28 14:12:19 +0100

    AddSwagger
    Swachbuckle Nuget Added to the API project
    Swagger services added to the DI-container
    Swagger UI enabled

---
+++ TheRestaurant/TheRestaurant.Presentation/Server/Config/Swagger/SwaggerServicesConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace TheRestaurant.Presentation.Server.Config.Swagger
+{
+    public static class SwaggerServicesConfig
+    {
+        public static IServiceCollection AddSwaggerServices(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
