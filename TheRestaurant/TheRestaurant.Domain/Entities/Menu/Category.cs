ffc1e0b46828956017da5947e2a434da1798d01a
Hafizullah Rahimi <62449891+HafizullahRahimi@users.noreply.github.com>
2023-11-29 11:11:18 +0100

    Add field IsActived to TblCategories

--- TheRestaurant/TheRestaurant.Domain/Entities/Menu/Category.cs
+++ TheRestaurant/TheRestaurant.Domain/Entities/Menu/Category.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsDeleted { get; set; }
 
         public ICollection<ProductCategory> MenuItemCategories { get; set; }
 
