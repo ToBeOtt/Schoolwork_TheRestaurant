d56ac2124159c18b5a7232175445512b57716bbb
ToBeOtt <119058608+ToBeOtt@users.noreply.github.com>
2023-11-30 14:25:24 +0100

    base

---
+++ TheRestaurant/TheRestaurant.Presentation/Client/Helpers/CheckRole.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace TheRestaurant.Presentation.Client.Helpers
+{
+    public class CheckRole
+    {
+        private readonly ServerAuthenticationStateProvider _authState;
+
+        public CheckRole(ServerAuthenticationStateProvider AuthState)
+        {
+            _authState = AuthState;
+        }
+
+        private bool IsAuthenticated;
+        public async Task<bool> CheckIfActiveRole()
+        {
+            // Subscribe to the authentication state changed event
+            _authState.AuthenticationStateChanged += HandleAuthenticationStateChanged;
+
+            return await UpdateSuccessState();
+
+        }     
+
+        private async Task<bool> UpdateSuccessState()
+        {
+            var user = await _authState.GetAuthenticationStateAsync();
+
+            if ((user.User.IsInRole("manager")) || (user.User.IsInRole("admin")))
+                IsAuthenticated = true;
+
+            else IsAuthenticated = false;
+
+            return IsAuthenticated;
+        }
+
+        private async void HandleAuthenticationStateChanged(Task<AuthenticationState> authenticationStateTask)
+        {
+            await UpdateSuccessState();
+        }
+    }
+}
