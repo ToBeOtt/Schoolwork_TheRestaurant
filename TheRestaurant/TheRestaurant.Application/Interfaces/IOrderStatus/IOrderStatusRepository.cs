feaec944d876bb8e977e2a6e3ae5c0aee74e4133
Hafizullah Rahimi <62449891+HafizullahRahimi@users.noreply.github.com>
2023-12-01 12:12:43 +0100

    Create IOrderStatusRepository and OrderStatusRepository

---
+++ TheRestaurant/TheRestaurant.Application/Interfaces/IOrderStatus/IOrderStatusRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheRestaurant.Domain.Entities.Menu;
+using TheRestaurant.Domain.Entities.OrderEntities;
+
+namespace TheRestaurant.Application.Interfaces.IOrderStatus
+{
+    public interface IOrderStatusRepository
+    {
+        Task<OrderStatus> GetAsync(int id);
+        Task<IReadOnlyList<OrderStatus>> GetAllAsync();
+        Task AddAsync(OrderStatus orderStatus);
+        Task UpdateAsync(OrderStatus orderStatus);
+        Task DeleteAsync(OrderStatus orderStatus);
+        Task DeleteAsync(int id);
+        Task<bool> Disable(int id);
+        Task<bool> Enable(int id);
+        bool Exists(int id);
+        Task SaveChangesAsync();
+    }
+}
---
+++ TheRestaurant/TheRestaurant.Common.Infrastructure/Repositories/OrderStatus/OrderStatusRepository.cs
@@ -0,0 +1,108 @@
+﻿using Common.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using TheRestaurant.Application.Interfaces.IOrderStatus;
+using TheRestaurant.Domain.Entities.Menu;
+
+namespace TheRestaurant.Common.Infrastructure.Repositories.OrderStatus
+{
+    public class OrderStatusRepository : IOrderStatusRepository
+    {
+        private readonly RestaurantDbContext _context;
+
+        public OrderStatusRepository(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+
+        //Get OrderStatus By Id
+        public async Task<Domain.Entities.OrderEntities.OrderStatus> GetAsync(int id)
+        {
+            var orderStatus = await _context.Set<Domain.Entities.OrderEntities.OrderStatus>().FirstOrDefaultAsync(c => c.Id == id);
+            return orderStatus;
+        }
+
+        //Get All OrderStatus
+        public async Task<IReadOnlyList<Domain.Entities.OrderEntities.OrderStatus>> GetAllAsync()
+        {
+            var list = await _context.OrderStatus.ToListAsync();
+            return list;
+        }
+
+        //Add new OrderStatus
+        public async Task AddAsync(Domain.Entities.OrderEntities.OrderStatus orderStatus)
+        {
+            await _context.AddAsync(orderStatus);
+            //await SaveChangesAsync();
+        }
+
+        //Update the OrderStatus
+        public async Task UpdateAsync(Domain.Entities.OrderEntities.OrderStatus orderStatus)
+        {
+            _context.Update(orderStatus);
+            //await SaveChangesAsync();
+        }
+
+        // Delete the OrderStatus
+        public async Task DeleteAsync(Domain.Entities.OrderEntities.OrderStatus orderStatus)
+        {
+            _context.OrderStatus.Remove(orderStatus);
+            //await SaveChangesAsync();
+        }
+
+        // Delete the OrderStatus By ID
+        public async Task DeleteAsync(int id)
+        {
+            var orderStatus = await GetAsync(id);
+            await DeleteAsync(orderStatus);
+        }
+
+
+        // Disable: Field IsDelete = True
+        public async Task<bool> Disable(int id)
+        {
+            try
+            {
+                var orderStatus = await GetAsync(id);
+                orderStatus.IsDeleted = true;
+                await UpdateAsync(orderStatus);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+
+        }
+
+        // Disable: Field IsDelete = False
+        public async Task<bool> Enable(int id)
+        {
+            try
+            {
+                var orderStatus = await GetAsync(id);
+                orderStatus.IsDeleted = true;
+                await UpdateAsync(orderStatus);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        }
+
+        //Exists
+        public bool Exists(int id)
+        {
+            return (_context.OrderStatus?.Any(o => o.Id == id)).GetValueOrDefault();
+        }
+
+        // SaveChanges
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
