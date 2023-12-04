07edf6ef7f26b89b0179a90631da81a758ce2409
ToBeOtt <119058608+ToBeOtt@users.noreply.github.com>
2023-12-04 14:16:14 +0100

    Finished
    - Added OrderDetails-page
    - Restructured user-nav to include orderdetails
          - Added nav icons for all user-nav links
    
    Added structure and planning for order-details, ready for backend-team to continue their work on.

---
+++ TheRestaurant/TheRestaurant.Presentation/Client/ClientServices/ClientOrderService.cs
@@ -0,0 +1,28 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+
+namespace TheRestaurant.Presentation.Client.ClientServices
+{
+    public class ClientOrderService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly NavigationManager _navigationManager;
+        private readonly ILocalStorageService _localStorage;
+
+        public ClientOrderService
+            (HttpClient httpClient,
+            NavigationManager navigationManager,
+            ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _navigationManager = navigationManager;
+            _localStorage = localStorage;
+        }
+
+        public async Task<int> CheckOrderStatus(int orderNr)
+        {
+            // Implementera relevant logik.
+            return 0;
+        }
+    }
+}
