7d8fea07cafe37f5253fc1d00a0bce30f1c2cc60
SimonJsv <124794619+SimonJsv@users.noreply.github.com>
2023-12-04 06:33:42 +0100

    Removed unnecessary files

--- TheRestaurant/TheRestaurant.Presentation/Client/Pages/Order/OrderDTO/CreateOrderRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TheRestaurant.Presentation.Client.Pages.Order.OrderDTO
-{
-    public class CreateOrderRequest
-    {
-        public DateTime OrderDate { get; set; }
-        public List<OrderProductDto> OrderItems { get; set; }
-    }
-
-}
