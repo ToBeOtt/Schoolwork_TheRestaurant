582c188967a40cf64e93bdfe34565f2876ae086b
SimonJsv <124794619+SimonJsv@users.noreply.github.com>
2023-12-04 06:33:55 +0100

    Added Necessary files again

---
+++ TheRestaurant/TheRestaurant.Application/DTOs/OrderDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheRestaurant.Application.DTOs
+{
+    public class OrderDTO
+    {
+        // Include only the properties you want to set via the form
+        public int Id { get; set; }
+        public DateTime OrderDate { get; set; }
+        // ... other properties like product details, quantity, etc.
+    }
+}
+
