6340bf98bd052c1e6563d609507fd77635c46695
SimonJsv <124794619+SimonJsv@users.noreply.github.com>
2023-12-07 13:37:19 +0100

    SeedOrder2

--- TheRestaurant/TheRestaurant.Common.Infrastructure/Data/SeedOrder2.cs
+++ TheRestaurant/TheRestaurant.Common.Infrastructure/Data/SeedOrder2.cs
@@ -8,7 +8,8 @@ namespace TheRestaurant.Common.Infrastructure.Data
 
         public static void SeedOrder2(this ModelBuilder modelBuilder)
         {
-            //TODO 3 pending orders, 3 active orders, 3 orders spread out on 3 different days.
+
+
             int hamburgerProductId = 1;
             int kebabProductId = 2;
             int salmonProductId = 3;
@@ -20,38 +21,79 @@ namespace TheRestaurant.Common.Infrastructure.Data
             int lambchopsProductId = 9;
             int blueberrypieProductId = 10;
 
-            // Seed Order
+            var orderStatus = 1;
+            var processingStatus = 2;
+            var completedStatus = 3;
+            var cancelledStatus = 4;
+            var activeStatus = 5;
+
             var pendingOrders = new List<Order>
             {
-                new Order { Id = 3, OrderDate = DateTime.Now, OrderStatusId = 1 },
-                new Order { Id = 4, OrderDate = DateTime.Now, OrderStatusId = 1 },
-                new Order { Id = 5, OrderDate = DateTime.Now, OrderStatusId = 1 }
-            };
-
-
-            var activeOrders = new List<Order>
-            {
-                new Order { Id = 6, OrderDate = DateTime.Now.AddDays(-1), OrderStatusId = 2 },
-                new Order { Id = 7, OrderDate = DateTime.Now.AddDays(-1), OrderStatusId = 2 },
-                new Order { Id = 8, OrderDate = DateTime.Now.AddDays(-1), OrderStatusId = 2 }
+                new Order
+                {
+                    Id = 31,
+                    OrderDate = DateTime.Now,
+                    OrderStatusId = orderStatus
+                },
+                new Order
+                {
+                    Id = 4,
+                    OrderDate = DateTime.Now,
+                    OrderStatusId = orderStatus
+                },
+                new Order
+                {
+                    Id = 5,
+                    OrderDate = DateTime.Now,
+                    OrderStatusId = orderStatus }
             };
 
             var spreadOrders = new List<Order>
             {
-                new Order { Id = 9, OrderDate = DateTime.Now.AddDays(-2), OrderStatusId = 1 },
-                new Order { Id = 10, OrderDate = DateTime.Now.AddDays(-3), OrderStatusId = 2 },
-                new Order { Id = 11, OrderDate = DateTime.Now.AddDays(-4), OrderStatusId = 1 }
+                new Order
+                {
+                    Id = 34,
+                    OrderDate = DateTime.Now.AddDays(-2),
+                    OrderStatusId = orderStatus
+                },
+                new Order
+                {
+                    Id = 10,
+                    OrderDate = DateTime.Now.AddDays(-3),
+                    OrderStatusId = completedStatus
+                },
+                new Order
+                {
+                    Id = 11,
+                    OrderDate = DateTime.Now.AddDays(-4),
+                    OrderStatusId = cancelledStatus
+                }
+            };
+            var activeOrders = new Order
+            {
+                Id = 6,
+                OrderDate = DateTime.Now.AddDays(-1),
+                OrderStatusId = activeStatus
+            };
+            var activeOrders2 = new Order
+            {
+                Id = 32,
+                OrderDate = DateTime.Now.AddDays(-1),
+                OrderStatusId = processingStatus
+            };
+            var activeOrders3 = new Order
+            {
+                Id = 33,
+                OrderDate = DateTime.Now.AddDays(-1),
+                OrderStatusId = activeStatus
             };
 
-            modelBuilder.Entity<Order>().HasData(pendingOrders, activeOrders, spreadOrders);
-
-
-
+            modelBuilder.Entity<Order>().HasData(activeOrders2, activeOrders, activeOrders3, spreadOrders, pendingOrders);
 
             var orderRowsForPendingOrders = new List<OrderRow>
             {
-                new OrderRow { Id = 16, OrderId = 3, ProductId = hamburgerProductId },
-                new OrderRow { Id = 17, OrderId = 3, ProductId = salmonProductId },
+                new OrderRow { Id = 16, OrderId = 31, ProductId = hamburgerProductId },
+                new OrderRow { Id = 17, OrderId = 31, ProductId = salmonProductId },
                 new OrderRow { Id = 18, OrderId = 4, ProductId = kebabProductId },
                 new OrderRow { Id = 19, OrderId = 4, ProductId = mozzarellaProductId },
                 new OrderRow { Id = 20, OrderId = 5, ProductId = caesarProductId }
@@ -61,17 +103,18 @@ namespace TheRestaurant.Common.Infrastructure.Data
             {
                 new OrderRow { Id = 21, OrderId = 6, ProductId = chocolateProductId },
                 new OrderRow { Id = 22, OrderId = 6, ProductId = veggocurryProductId },
-                new OrderRow { Id = 23, OrderId = 7, ProductId = tomyumsoupProductId },
-                new OrderRow { Id = 24, OrderId = 7, ProductId = lambchopsProductId },
-                new OrderRow { Id = 25, OrderId = 8, ProductId = blueberrypieProductId }
+                new OrderRow { Id = 23, OrderId = 32, ProductId = tomyumsoupProductId },
+                new OrderRow { Id = 24, OrderId = 32, ProductId = lambchopsProductId },
+                new OrderRow { Id = 25, OrderId = 33, ProductId = blueberrypieProductId }
             };
 
             var orderRowsForSpreadOrders = new List<OrderRow>
             {
-                new OrderRow { Id = 26, OrderId = 9, ProductId = hamburgerProductId },
+                new OrderRow { Id = 26, OrderId = 34, ProductId = hamburgerProductId },
                 new OrderRow { Id = 27, OrderId = 10, ProductId = salmonProductId },
                 new OrderRow { Id = 28, OrderId = 11, ProductId = kebabProductId }
             };
+
 
             modelBuilder.Entity<OrderRow>().HasData(orderRowsForPendingOrders.ToArray());
             modelBuilder.Entity<OrderRow>().HasData(orderRowsForActiveOrders.ToArray());
