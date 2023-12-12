8c51ddc7d97bd589a7b665c7ae64c9cf5ee02956
znooker <znooker_18@hotmail.com>
2023-12-12 15:49:38 +0100

    Display ordercomment on pending order
    Display ordercomment on pending order

--- TheRestaurant/TheRestaurant.Contracts/Responses/Orders/PendingOrdersResponse.cs
+++ TheRestaurant/TheRestaurant.Contracts/Responses/Orders/PendingOrdersResponse.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace TheRestaurant.Contracts.Responses.Orders
 {
-    public record PendingOrdersResponse( int OrderNr,
+    public record PendingOrdersResponse( 
+        int OrderNr,
+        string? Comment,
         DateTime DateTimeOfOrder,
         List<ProductAndQuantity> ProductAndQuantity,
         string? EmployeeName
