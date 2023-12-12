095ccf8afa8c7908625ff096ba02dba65d9da69e
Sheldozer <125883340+Sheldozer@users.noreply.github.com>
2023-12-12 13:53:13 +0100

    Service, Repository & API endpoints implemtned
    Service, Repository & API endpoints implemtned

--- TheRestaurant/TheRestaurant.Application/Interfaces/IVat/IVatRepository.cs
+++ TheRestaurant/TheRestaurant.Application/Interfaces/IVat/IVatRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheRestaurant.Domain.Entities.Menu;
 
 namespace TheRestaurant.Application.Interfaces.IVat
 {
     public interface IVatRepository
     {
+        Task<List<VAT>> GetAllAsync();
+        Task<VAT> GetByIdAsync(int id);
+        Task<VAT> AddAsync(VAT vat);
+        Task UpdateAsync(VAT vat);
+        Task DeleteAsync(int id);
     }
 }
--- TheRestaurant/TheRestaurant.Presentation/Server/Controllers/Admin/VatController.cs
+++ TheRestaurant/TheRestaurant.Presentation/Server/Controllers/Admin/VatController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TheRestaurant.Application.Interfaces.IVat;
+using TheRestaurant.Contracts.Requests.Vat;
 
 namespace TheRestaurant.Presentation.Server.Controllers.Admin
 {
@@ -7,5 +9,100 @@ namespace TheRestaurant.Presentation.Server.Controllers.Admin
     [ApiController]
     public class VatController : ControllerBase
     {
+        private readonly IVatService _vatService;
+
+        public VatController(IVatService vatService)
+        {
+            _vatService = vatService;
+        }
+
+        // Get all
+        [HttpGet]
+        public async Task<IActionResult> GetAllVats()
+        {
+            try
+            {
+                var vats = await _vatService.GetAllVatsAsync();
+                return Ok(vats);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Ett fel uppstod när momsen hämtades: {ex.Message}");
+            }
+            
+        }
+
+        // Get by id
+        [HttpGet("id")]
+        public async Task<IActionResult> GetVatById(int id)
+        {
+            try
+            {
+                var vat = await _vatService.GetVatByIdAsync(id);
+                if (vat == null)
+                {
+                    return NotFound();
+                }
+                return Ok(vat);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, $"Ett fel uppstod när momsen hämtades: {ex.Message}");
+            }
+            
+        }
+
+        // Create 
+        [HttpPost]
+        public async Task<IActionResult> CreateVat(VatRequest vatRequest)
+        {
+            try
+            {
+
+                var createdVat = await _vatService.CreateVatAsync(vatRequest);
+                return CreatedAtAction(nameof(GetVatById), new { id = createdVat.Id }, createdVat);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, $"Ett fel uppstod när momsen skapdes: {ex.Message}");
+            }
+        }
+
+        // Update
+        public async Task<IActionResult> UpdateVat(int id, VatRequest vatRequest)
+        {
+            try
+            {
+                var updatedVat = await _vatService.UpdateVatAsync(id, vatRequest);
+                if (updatedVat == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(updatedVat);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Ett fel uppstod när moms skulle uppdateras: {ex.Message}");
+            }
+        }
+
+        // Delete
+        public async Task<IActionResult> DeleteVat(int id)
+        {
+            try
+            {
+                await _vatService.DeleteVatAsync(id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(500, $"Ett fel uppstod när moms skulle raderas: {ex.Message}");
+            }
+
+        }
     }
 }
