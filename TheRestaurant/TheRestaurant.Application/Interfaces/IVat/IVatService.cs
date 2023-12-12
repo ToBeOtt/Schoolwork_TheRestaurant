a25db977c5d66acaa98c193191b5f5689ba87ef5
Sheldozer <125883340+Sheldozer@users.noreply.github.com>
2023-12-12 14:00:25 +0100

    Service interface

--- TheRestaurant/TheRestaurant.Application/Interfaces/IVat/IVatService.cs
+++ TheRestaurant/TheRestaurant.Application/Interfaces/IVat/IVatService.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheRestaurant.Contracts.Requests.Vat;
+using TheRestaurant.Domain.Entities.Menu;
 
 namespace TheRestaurant.Application.Interfaces.IVat
 {
     public interface IVatService
     {
+        Task<List<VAT>> GetAllVatsAsync();
+        Task<VAT> GetVatByIdAsync(int id);
+        Task<VAT> CreateVatAsync(VatRequest vatRequest);
+        Task<VAT> UpdateVatAsync(int id, VatRequest vatRequest);
+        Task DeleteVatAsync(int id);
     }
 }
