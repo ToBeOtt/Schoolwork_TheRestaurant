1027ff449cc536eff5c45f7ec8f687ec6257eb43
znooker <znooker_18@hotmail.com>
2023-12-18 17:14:59 +0100

    UpdateProductsVatAndPrices
    UpdateProductsVatAndPrices

---
+++ TheRestaurant/TheRestaurant.Common.Infrastructure/Migrations/20231218161028_UpdateProductPricesAndVat.cs
@@ -0,0 +1,411 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheRestaurant.Common.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductPricesAndVat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 17, 10, 28, 221, DateTimeKind.Local).AddTicks(233));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 17, 10, 28, 221, DateTimeKind.Local).AddTicks(474));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 17, 10, 28, 221, DateTimeKind.Local).AddTicks(489));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 17, 10, 28, 221, DateTimeKind.Local).AddTicks(492));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 17, 10, 28, 221, DateTimeKind.Local).AddTicks(522));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 15, 17, 10, 28, 221, DateTimeKind.Local).AddTicks(499));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 14, 17, 10, 28, 221, DateTimeKind.Local).AddTicks(502));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 31,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 17, 10, 28, 221, DateTimeKind.Local).AddTicks(484));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 32,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 17, 10, 28, 221, DateTimeKind.Local).AddTicks(534));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 33,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 17, 10, 28, 221, DateTimeKind.Local).AddTicks(539));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 34,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 16, 17, 10, 28, 221, DateTimeKind.Local).AddTicks(495));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Price",
+                value: 88.480000000000004);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Price",
+                value: 110.88);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Price",
+                value: 144.47999999999999);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Price",
+                value: 99.680000000000007);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Price",
+                value: 54.880000000000003);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Price",
+                value: 77.280000000000001);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Price",
+                value: 122.08);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Price",
+                value: 133.28);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Price",
+                value: 166.88);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Price",
+                value: 66.079999999999998);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "Price",
+                value: 22.399999999999999);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Price",
+                value: 22.399999999999999);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 15,
+                columns: new[] { "Price", "VATId" },
+                values: new object[] { 150.0, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "Price",
+                value: 104.94);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 17,
+                columns: new[] { "Price", "VATId" },
+                values: new object[] { 562.5, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 18,
+                columns: new[] { "Price", "VATId" },
+                values: new object[] { 250.0, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 19,
+                columns: new[] { "Price", "VATId" },
+                values: new object[] { 250.0, 4 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 17, 4, 5, 199, DateTimeKind.Local).AddTicks(5892));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 17, 4, 5, 199, DateTimeKind.Local).AddTicks(5935));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 17, 4, 5, 199, DateTimeKind.Local).AddTicks(5946));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 17, 4, 5, 199, DateTimeKind.Local).AddTicks(5949));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 17, 4, 5, 199, DateTimeKind.Local).AddTicks(5957));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 15, 17, 4, 5, 199, DateTimeKind.Local).AddTicks(5953));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 14, 17, 4, 5, 199, DateTimeKind.Local).AddTicks(5955));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 31,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 17, 4, 5, 199, DateTimeKind.Local).AddTicks(5943));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 32,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 17, 4, 5, 199, DateTimeKind.Local).AddTicks(5960));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 33,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 17, 4, 5, 199, DateTimeKind.Local).AddTicks(5962));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 34,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 16, 17, 4, 5, 199, DateTimeKind.Local).AddTicks(5951));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Price",
+                value: 88.0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Price",
+                value: 111.0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Price",
+                value: 144.0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Price",
+                value: 100.0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Price",
+                value: 55.0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Price",
+                value: 77.0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Price",
+                value: 122.0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Price",
+                value: 133.0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Price",
+                value: 167.0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Price",
+                value: 66.0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "Price",
+                value: 22.0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 12,
+                column: "Price",
+                value: 22.0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 15,
+                columns: new[] { "Price", "VATId" },
+                values: new object[] { 127.0, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 16,
+                column: "Price",
+                value: 105.0);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 17,
+                columns: new[] { "Price", "VATId" },
+                values: new object[] { 477.0, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 18,
+                columns: new[] { "Price", "VATId" },
+                values: new object[] { 212.0, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 19,
+                columns: new[] { "Price", "VATId" },
+                values: new object[] { 212.0, 2 });
+        }
+    }
+}
