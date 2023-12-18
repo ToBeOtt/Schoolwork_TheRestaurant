9870429aa0d7bb9fb4a492f55adf251af730d2b2
Hafizullah Rahimi <62449891+HafizullahRahimi@users.noreply.github.com>
2023-12-18 20:21:00 +0100

    Add OrderStatusService, IOrderStatusService and OrderStatusDto

---
+++ TheRestaurant/TheRestaurant.Application/Interfaces/IOrderStatus/IOrderStatusService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheRestaurant.Contracts.DTOs;
+
+namespace TheRestaurant.Application.Interfaces.IOrderStatus
+{
+    public interface IOrderStatusService
+    {
+        Task<OrderStatusDto> GetAsync(int id);
+        Task<IReadOnlyList<OrderStatusDto>> GetAllAsync();
+        Task<bool> AddAsync(OrderStatusDto orderStatusDto);
+        Task<bool> UpdateAsync(OrderStatusDto orderStatusDto);
+        Task<bool> DeleteAsync(int id);
+        bool Exists(int id);
+    }
+}
