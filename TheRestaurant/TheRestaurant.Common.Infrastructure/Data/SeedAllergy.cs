26f72044c0a36fec847519ef2076298e65d15874
znooker <znooker_18@hotmail.com>
2023-12-18 16:16:27 +0100

    AllergyUpdate
    Updated to Swe

--- TheRestaurant/TheRestaurant.Common.Infrastructure/Data/SeedAllergy.cs
+++ TheRestaurant/TheRestaurant.Common.Infrastructure/Data/SeedAllergy.cs
@@ -13,16 +13,16 @@ namespace TheRestaurant.Common.Infrastructure.Data
         public static void AllergySeed(this ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Allergy>().HasData(
-                new Allergy { Id = 1, Name = "Peanuts" },
-                new Allergy { Id = 2, Name = "Shellfish" },
-                new Allergy { Id = 3, Name = "Milk" },
-                new Allergy { Id = 4, Name = "Eggs" },
-                new Allergy { Id = 5, Name = "Fish" },
-                new Allergy { Id = 6, Name = "Tree nuts" },
-                new Allergy { Id = 7, Name = "Wheat" },
-                new Allergy { Id = 8, Name = "Soy" },
-                new Allergy { Id = 9, Name = "Sesame" },
-                new Allergy { Id = 10, Name = "Sulfites" }
+                new Allergy { Id = 1, Name = "Jordnötter" },
+                new Allergy { Id = 2, Name = "Skaldjur" },
+                new Allergy { Id = 3, Name = "Mjölk" },
+                new Allergy { Id = 4, Name = "Ägg" },
+                new Allergy { Id = 5, Name = "Fisk" },
+                new Allergy { Id = 6, Name = "Nötter" },
+                new Allergy { Id = 7, Name = "Vete" },
+                new Allergy { Id = 8, Name = "Soja" },
+                new Allergy { Id = 9, Name = "Sesamfrö " },
+                new Allergy { Id = 10, Name = "Sulfit" }
             );
         }
     }
---
+++ TheRestaurant/TheRestaurant.Common.Infrastructure/Migrations/20231218151417_AllergiesUpdateToSwedish.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheRestaurant.Common.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AllergiesUpdateToSwedish : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Jordnötter");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Skaldjur");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Mjölk");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Name",
+                value: "Ägg");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Name",
+                value: "Fisk");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Name",
+                value: "Nötter");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Name",
+                value: "Vete");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Name",
+                value: "Soja");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Name",
+                value: "Sesamfrö ");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Name",
+                value: "Sulfit");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 16, 14, 17, 625, DateTimeKind.Local).AddTicks(7914));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 16, 14, 17, 625, DateTimeKind.Local).AddTicks(7954));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 16, 14, 17, 625, DateTimeKind.Local).AddTicks(7966));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 16, 14, 17, 625, DateTimeKind.Local).AddTicks(7968));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 16, 14, 17, 625, DateTimeKind.Local).AddTicks(7977));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 15, 16, 14, 17, 625, DateTimeKind.Local).AddTicks(7973));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 14, 16, 14, 17, 625, DateTimeKind.Local).AddTicks(7975));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 31,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 16, 14, 17, 625, DateTimeKind.Local).AddTicks(7961));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 32,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 16, 14, 17, 625, DateTimeKind.Local).AddTicks(7980));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 33,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 16, 14, 17, 625, DateTimeKind.Local).AddTicks(7982));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 34,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 16, 16, 14, 17, 625, DateTimeKind.Local).AddTicks(7970));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Peanuts");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Shellfish");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Milk");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Name",
+                value: "Eggs");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "Name",
+                value: "Fish");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Name",
+                value: "Tree nuts");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Name",
+                value: "Wheat");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Name",
+                value: "Soy");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 9,
+                column: "Name",
+                value: "Sesame");
+
+            migrationBuilder.UpdateData(
+                table: "Allergies",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Name",
+                value: "Sulfites");
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 16, 3, 33, 962, DateTimeKind.Local).AddTicks(1513));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 16, 3, 33, 962, DateTimeKind.Local).AddTicks(1553));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 16, 3, 33, 962, DateTimeKind.Local).AddTicks(1567));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 16, 3, 33, 962, DateTimeKind.Local).AddTicks(1569));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 16, 3, 33, 962, DateTimeKind.Local).AddTicks(1578));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 15, 16, 3, 33, 962, DateTimeKind.Local).AddTicks(1574));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 11,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 14, 16, 3, 33, 962, DateTimeKind.Local).AddTicks(1576));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 31,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 18, 16, 3, 33, 962, DateTimeKind.Local).AddTicks(1560));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 32,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 16, 3, 33, 962, DateTimeKind.Local).AddTicks(1580));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 33,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 17, 16, 3, 33, 962, DateTimeKind.Local).AddTicks(1582));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "Id",
+                keyValue: 34,
+                column: "OrderDate",
+                value: new DateTime(2023, 12, 16, 16, 3, 33, 962, DateTimeKind.Local).AddTicks(1571));
+        }
+    }
+}
