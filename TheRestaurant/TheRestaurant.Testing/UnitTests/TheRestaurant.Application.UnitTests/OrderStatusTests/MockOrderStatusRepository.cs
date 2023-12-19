8e2db4da9dc3a468c76336304c1a8fbf98862960
Hafizullah Rahimi <62449891+HafizullahRahimi@users.noreply.github.com>
2023-12-19 18:55:53 +0100

    Add OrderStatusServiceTests

---
+++ TheRestaurant/TheRestaurant.Testing/UnitTests/TheRestaurant.Application.UnitTests/OrderStatusTests/MockOrderStatusRepository.cs
@@ -0,0 +1,53 @@
+﻿using Moq;
+using TheRestaurant.Application.Interfaces.ICategory;
+using TheRestaurant.Application.Interfaces.IOrderStatus;
+using TheRestaurant.Domain.Entities.Menu;
+using TheRestaurant.Domain.Entities.Orders;
+
+namespace TheRestaurant.Testing.UnitTests.TheRestaurant.Application.UnitTests.OrderStatusTests
+{
+    public static class MockOrderStatusRepository
+    {
+        public static Mock<IOrderStatusRepository> GetMock()
+        {
+            var orderStatusList = new List<OrderStatus>()
+            {
+                 new OrderStatus { Id = 1, Status = "Pending", IsDeleted = false },
+                new OrderStatus { Id = 2, Status = "Processing", IsDeleted = false },
+                new OrderStatus { Id = 3, Status = "Delivered", IsDeleted = false },
+                new OrderStatus { Id = 4, Status = "Cancelled", IsDeleted = false }
+            };
+
+
+            var mockRepo = new Mock<IOrderStatusRepository>();
+
+            // GetAllAsync
+            mockRepo.Setup(r => r.GetAllAsync()).ReturnsAsync(orderStatusList);
+
+
+            // GetByIdAsync
+            mockRepo.Setup(r => r.GetAsync(It.IsAny<int>())).ReturnsAsync((int id) =>
+            {
+                var result = from c in orderStatusList where c.Id == id select c;
+                var category = new OrderStatus
+                {
+                    Id = id,
+                    Status = result.First().Status,
+                    IsDeleted = result.First().IsDeleted
+                };
+
+                return category;
+            });
+
+
+            // UpdateAsync
+            mockRepo.Setup(r => r.UpdateAsync(It.IsAny<OrderStatus>())).ReturnsAsync((OrderStatus orderStatus) =>
+            {
+                return orderStatus;
+            });
+
+
+            return mockRepo;
+        }
+    }
+}
---
+++ TheRestaurant/TheRestaurant.Testing/UnitTests/TheRestaurant.Application.UnitTests/OrderStatusTests/OrderStatusServiceTests.cs
@@ -0,0 +1,68 @@
+﻿using Moq;
+using TheRestaurant.Application.Interfaces.IOrderStatus;
+using TheRestaurant.Application.Services;
+using TheRestaurant.Contracts.DTOs;
+using TheRestaurant.Testing.UnitTests.TheRestaurant.Application.UnitTests.CategoryTests;
+
+namespace TheRestaurant.Testing.UnitTests.TheRestaurant.Application.UnitTests.OrderStatusTests
+{
+    public class OrderStatusServiceTests
+    {
+        readonly Mock<IOrderStatusRepository> _mockRepository;
+        public OrderStatusServiceTests()
+        {
+            _mockRepository = MockOrderStatusRepository.GetMock();
+        }
+
+
+        [Fact]
+        public async Task GetAllAsyncTest()
+        {
+            // Arrange
+            var orderStatusService = new OrderStatusService(_mockRepository.Object);
+
+            // Act
+            var result = await orderStatusService.GetAllAsync();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(4, result.Count);
+
+        }
+
+
+        [Fact]
+        public async Task GetAsyncTest()
+        {
+            // Arrange
+            var orderStatusService = new OrderStatusService(_mockRepository.Object);
+
+            // Act
+            var result = await orderStatusService.GetAsync(3);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("Delivered", result.Status);
+            Assert.Equal(3, result.Id);
+
+        }
+
+
+        [Fact]
+        public async Task UpdateAsyncTest()
+        {
+            // Arrange
+            var orderStatusService = new OrderStatusService(_mockRepository.Object);
+
+            var orderStatus = new OrderStatusDto { Id = 1, Status = "Active", IsDeleted = true };
+
+            //// Act
+            var updated = await orderStatusService.UpdateAsync(orderStatus);
+
+            // Assert
+            Assert.NotNull(updated);
+            Assert.Equal(true, updated);
+
+        }
+    }
+}
